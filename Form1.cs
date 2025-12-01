bb682d5325c7e7defbeab7c06b95ca55a5a7ffa2
Elif <198382528+Elif999@users.noreply.github.com>
2025-12-01 22:13:12 +0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication15
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public class Ort
+        {
+            public int yazili1, yazili2;
+            public int hesapla(int y1, int y2)
+            {
+                int son = (y1 + y2) / 2;
+                return son;
+
+            }
+            public int not1, not2;
+            public void hesap(int n1, int n2)
+            {
+                int sonuc = (n1 + n2) / 2;
+                if (sonuc < 39)
+                {
+                    MessageBox.Show("zayıf");
+                }
+                else if (sonuc >= 40 && sonuc < 50)
+                {
+                    MessageBox.Show("orta");
+                }
+                else if (sonuc >= 50 && sonuc < 60)
+                {
+                    MessageBox.Show("iyi");
+                }
+                else if (sonuc >= 60 && sonuc < 75)
+                {
+                    MessageBox.Show("çok iyi");
+                }
+                else
+                {
+                    MessageBox.Show("mükemmel");
+                }
+            }
+        }
+        Ort o = new Ort();
+        private void button1_Click(object sender, EventArgs e)
+        {
+         
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+           o.yazili1 = Convert.ToInt32(textBox1.Text);
+            o.yazili2 = Convert.ToInt32(textBox2.Text);
+           int son= o.hesapla(o.yazili1, o.yazili2);
+            if (son < 39)
+            {
+                listBox1.Items.Add("zayıf" + son);
+            }
+            else if (son >= 40 && son < 50)
+            {
+                listBox1.Items.Add("orta" + son);
+            }
+            else if (son >= 50 && son < 60)
+            {
+                listBox1.Items.Add("iyi" + son);
+            }
+            else if (son >= 60 && son < 75)
+            {
+                listBox1.Items.Add("çok iyi" + son);
+            }
+            else
+            {
+                listBox1.Items.Add("mükemmel" + son);
+            }
+        }
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            o.not1 = Convert.ToInt32(textBox1.Text);
+            o.not2 = Convert.ToInt32(textBox2.Text);
+            o.hesap(o.not1, o.not2);
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Remove(listBox1.SelectedItem);
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+        }
+        private void button5_Click(object sender, EventArgs e)
+        {
+            kullanıcı_giriş k = new kullanıcı_giriş();
+            k.Show();
+            this.Hide();
+        }
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
+
---
+++ kullanıcı giriş.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication15
+{
+    public partial class kullanıcı_giriş : Form
+    {
+        public kullanıcı_giriş()
+        {
+            InitializeComponent();
+        }
+        class Hesap
+        {
+            public string Isim { get; set; }
+            public string Sifre { get; set; }
+            public string Tel { get; set; }
+
+            public Hesap(string isim, string sifre, string tel)
+            {
+                Isim = isim;
+                Sifre = sifre;
+                Tel = tel;
+            }
+
+            public bool SifreKontrol(string sifre)
+            {
+                return Sifre == sifre;
+            }
+        }
+
+        List<Hesap> hesaplar = new List<Hesap>();
+        Hesap aktifHesap = null;
+
+        private void kullanıcı_giriş_Load(object sender, EventArgs e)
+        {
+            // Kayıt ekranı gibi başlasın
+            label3.Visible = false;
+            label4.Visible = false;
+            label6.Visible = false;
+
+            label7.Visible = true;     // 📌 Telefon başlık label'ı
+            textBox4.Visible = true;   // 📌 Telefon textbox'ı
+
+            button2.Visible = false;   // Giriş butonu gizli başlasın
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string isim = textBox1.Text.Trim();
+            string sifre = textBox2.Text.Trim();
+            string tel = textBox4.Text.Trim();
+
+            if (isim == "" || sifre == "" || tel == "")
+            {
+                MessageBox.Show("Lütfen tüm alanları doldurun.");
+                return;
+            }
+
+            if (sifre.Length < 6)
+            {
+                MessageBox.Show("Şifreniz en az 6 karakter olmalıdır!", "Uyarı");
+                return;
+            }
+
+            aktifHesap = new Hesap(isim, sifre, tel);
+            hesaplar.Add(aktifHesap);
+
+            MessageBox.Show("Şifreniz başarıyla oluşturuldu!", "Bilgi");
+
+            label4.Text = aktifHesap.Isim;
+            label3.Text = "******";
+            label6.Text = aktifHesap.Tel;
+
+            label4.Visible = true;
+            label3.Visible = true;
+            label6.Visible = true;
+
+            TemizleGirisEkrani();
+            button1.Visible = false;
+            button2.Visible = true;
+
+            textBox4.Visible = false;
+            label7.Visible = false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string isim = textBox1.Text.Trim();
+            string sifre = textBox2.Text.Trim();
+
+            foreach (var hesap in hesaplar)
+            {
+                if (hesap.Isim == isim && hesap.SifreKontrol(sifre))
+                {
+                    MessageBox.Show("Giriş başarılı!", "Bilgi");
+                    Form1 fr1 = new Form1();
+                    fr1.Show();
+                    this.Hide();
+                    return;
+                }
+            }
+
+            MessageBox.Show("Kullanıcı adı veya şifre hatalı!", "Hata");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (button4.Text == "Şifremi Unuttum")
+            {
+                // Giriş alanlarını gizle
+                textBox1.Visible = false;
+                textBox2.Visible = false;
+                label1.Visible = false;
+                label2.Visible = false;
+                button2.Visible = false;
+
+                // Tel alanını aç
+                label7.Visible = true;
+                textBox4.Visible = true;
+                button4.Text = "Yenile";
+            }
+            else
+            {
+                string girilenTel = textBox4.Text.Trim();
+                foreach (var hesap in hesaplar)
+                {
+                    if (hesap.Tel == girilenTel)
+                    {
+                        aktifHesap = hesap;
+                        MessageBox.Show("Şifrenizi yeniden oluşturabilirsiniz.");
+
+                        // Yeniden kayıt ekranı aç
+                        textBox1.Visible = true;
+                        textBox2.Visible = true;
+                        label1.Visible = true;
+                        label2.Visible = true;
+                        button1.Visible = true;
+
+                        button4.Visible = false; // Artık yeniden kayıt yapabilir
+                        return;
+                    }
+                }
+
+                MessageBox.Show("Telefon numarası eşleşmedi.", "Hata");
+            }
+        }
+
+        private void TemizleGirisEkrani()
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox4.Clear();
+        }
+    }
+   
+    }
+
+
